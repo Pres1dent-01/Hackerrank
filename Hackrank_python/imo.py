a085c072dac1591cd92253491e9d870ed57410be
Abhijit Dhara <51172366+cybertron21@users.noreply.github.com>
2020-05-10 00:52:29 +0530

    Add files via upload

---
+++ Hackrank_python/imo.py
@@ -0,0 +1,5 @@
+"""This question involves more maths than programming to give a slight dive into the world of maths"""
+"""print((10**(i)//9)*i)"""
+"""This is a IMO question"""
+for i in range(0, int(input())-1):
+	print([1, 22, 333, 4444, 55555, 666666, 7777777, 88888888, 999999999][i])
